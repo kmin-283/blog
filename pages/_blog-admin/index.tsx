3752a1353859f9ffbf0135fb2285a19e7973f9ab
민경한 <kmin@mingyeonghans-MacBook-Air.local>
2021-08-30 19:22:18 +0900

    feat: login기능 구현

--- pages/_blog-admin/index.tsx
+++ pages/_blog-admin/index.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import React from "react";
+import { useSession } from "next-auth/client";
+import Login from "@/components/login/login";
 
-const Admin = () => <h1>admin page 통계적인 자료가 보여지면 좋을 것 같다</h1>;
-
+const Admin = () => {
+  const [session] = useSession();
+  return (
+    <>
+      {!session && <Login />}
+      {session && <h1>admin 페이지. 통계적인 자료가 보여지면 좋을 것 같다.</h1>}
+    </>
+  );
+};
 export default Admin;
--- pages/_blog-login/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Login = () => <h1>login page 로그인에 성공하면 admin으로 가자</h1>;
-
-export default Login;
