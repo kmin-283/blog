4b53a37a74d31e4c7dfc5c96c239151939559922
kmin-283 <rudgks114@gmail.com>
2021-09-07 02:09:32 +0900

    feat: post 컴포넌트에서 db에 저장된 내용 불러오기

--- components/posts/posts.tsx
+++ components/posts/posts.tsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
+import { IPost } from "../../models/post";
 
 const PostsComp = () => {
+  const [posts, setPosts] = useState<IPost[]>([]);
+  useEffect(() => {
+    const getPosts = async () => {
+      const response = await fetch("/api/posts", {
+        method: "GET",
+      });
+      if (response.ok) {
+        const { data }: { data: IPost[] } = await response.json();
+        setPosts(data);
+      }
+    };
+    getPosts();
+  }, []);
+
   return (
     <>
       <Link href={"/_blog-admin/write"}>
         <a>게시글 작성</a>
       </Link>
       <ul>
-        <li data-mainentity="">
-          <h2 data-name="">1</h2>
-          <p data-description="">desc1</p>
-        </li>
-        <li data-mainentity="">2</li>
-        <li data-mainentity="">3</li>
-        <li data-mainentity="">4</li>
+        {posts.map((post) => {
+          return (
+            <li key={post.title} data-mainentity="">
+              <h2 data-name="">{post.title}</h2>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
