78762712d08a5c8ca3ad61a4604847c0ced26de7
yalpal yappala <rudgks114@gmail.com>
2021-10-03 01:25:46 +0900

    feat: postcard 나머지 구현

--- utils/time.ts
+++ utils/time.ts
@@ -1,8 +1,7 @@
 const convertToKRDate = (isoDate: string) => {
   const [yymmdd, time] = isoDate.split("T");
   const [year, month, day] = yymmdd.split("-");
-  const [hour, minutes] = time.slice(0, 5).split(":");
-  return `${year}년 ${month}월 ${day}일 ${hour}시 ${minutes}분`;
+  return `${year}년 ${month}월 ${day}일`;
 };
 
 export { convertToKRDate };
