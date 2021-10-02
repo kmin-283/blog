b51d43651b0e413e3390e399cb78cb706e83e885
yalpal yappala <rudgks114@gmail.com>
2021-10-02 21:01:14 +0900

    feat: footer 스타일링2

--- components/layout/footer/footer.tsx
+++ components/layout/footer/footer.tsx
@@ -6,11 +6,21 @@ import { FaGithub } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <Link aria-label="go to github" href="https://github.com/kmin-283">
-        <a>
-          <FaGithub size="1.5em" color="white" />
-        </a>
-      </Link>
+      <div className={`${styles.socialIcon} social-icon`}>
+        <Link aria-label="go to github" href="https://github.com/kmin-283">
+          <a>
+            <FaGithub size="2em" />
+          </a>
+        </Link>
+      </div>
+      <div className={`${styles.sitemap} sitemap`}>
+        <Link href="/">
+          <a>Blog</a>
+        </Link>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </div>
       <div>
         <small>Copyright &copy; 2021 kmin. All rights reserved.</small>
       </div>
