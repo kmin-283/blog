20eea0e4875c3268ab68e16773ef55126f979067
yalpal yappala <rudgks114@gmail.com>
2021-10-21 23:26:39 +0900

    refactor: description 한글로 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import { ReactElement } from "react";
+import {ReactElement} from "react";
 import Head from "next/head";
-import { GetStaticPropsResult } from "next";
-import { NextPageWithLayout } from "./_app";
+import {GetStaticPropsResult} from "next";
+import {NextPageWithLayout} from "./_app";
 import Header from "@/components/layout/header/header";
 import Footer from "@/components/layout/footer/footer";
 import PostCard from "@/components/PostCard/postCard";
-import Post, { IPost } from "../models/post";
+import Post, {IPost} from "../models/post";
 import styles from "./index.module.css";
 import connectDB from "@/utils/mongodb";
 
@@ -13,20 +13,20 @@ export interface HomeProps {
   posts: IPost[];
 }
 
-const Home: NextPageWithLayout<HomeProps> = ({ posts }) => {
+const Home: NextPageWithLayout<HomeProps> = ({posts}) => {
   return (
     <div>
       <Head>
         <title>kmin</title>
         <meta
           name="description"
-          content="Website About kmin. In Blog page, write a articles on developing."
+          content="kmin의 블로그입니다. 개발을 진행하며 학습한 내용들을 작성하려고 합니다."
         />
       </Head>
       <main>
         <div className={styles.posts}>
           {posts.map((post) => {
-            return <PostCard key={post._id} post={post} />;
+            return <PostCard key={post._id} post={post}/>;
           })}
         </div>
       </main>
@@ -37,21 +37,19 @@ const Home: NextPageWithLayout<HomeProps> = ({ posts }) => {
 Home.getLayout = function getLayout(page: ReactElement) {
   return (
     <>
-      <Header />
+      <Header/>
       {page}
-      <Footer />
+      <Footer/>
     </>
   );
 };
 
 export default Home;
 
-export const getStaticProps = async (): Promise<
-  GetStaticPropsResult<HomeProps>
-> => {
+export const getStaticProps = async (): Promise<GetStaticPropsResult<HomeProps>> => {
   await connectDB();
   const posts = JSON.stringify(await Post.find({}));
-
+  
   if (posts) {
     return {
       props: {
