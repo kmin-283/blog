2419e8b454f023dc11f252a5159ad83f8fcc1c24
kmin-283 <rudgks1203@naver.com>
2021-11-01 23:20:01 +0900

    fix: image 두개 연달아 생성할 경우 발생한 에러 해결
    image를 두개 연달아서 생성하면 정규표현식 오류로 잘못된
    이미지가 들어가는 문제가 있었음
    
    정규표현식을 수정하여 해결

--- project/utils/imageUpload.ts
+++ project/utils/imageUpload.ts
@@ -1,18 +1,7 @@
 const getThumbnail = (markdown: string) => {
-  const imageTag = (markdown as string).match(/!\[.*\]\((.*)\)/);
-  if (imageTag && imageTag.length > 0) {
-    let imageSource = imageTag[0].match(/\([^\s]*\)?/);
-    if (!imageSource) {
-      return "";
-    }
-    if (imageSource[0][imageSource.length - 1] !== ')') {
-      return imageSource[0].slice(1);
-    } else {
-      return imageSource[0].slice(1, -1);
-    }
-  } else {
-    return "";
-  }
+  const thumbnailTag = markdown.match(/!\[[^()[\]]*\]\([^)]+\)/)![0];
+  const thumbnail = thumbnailTag.match(/\([^\s]*\)?/)![0];
+  return thumbnail[thumbnail.length - 1] !== ')' ? thumbnail.slice(1) : thumbnail.slice(1, -1);
 };
 
 export {getThumbnail};
