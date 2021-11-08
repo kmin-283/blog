ae7cdf3ce826a717dbfbdd92c98bae4ce272df23
yalpal yappala <rudgks114@gmail.com>
2021-11-08 21:34:29 +0900

    fix: image sitemap 에러 수정
    sitemap.xml 상단에 <xmlns:image ~~>가 없어서 생긴 오류였다.

--- project/utils/generateJsonLD.ts
+++ project/utils/generateJsonLD.ts
@@ -1,37 +1,44 @@
-const generateJsonLD = (
-  {
-    title, description, thumbnail, updatedAt, tags
-  }: {
-    title: string, description: string, thumbnail: string, updatedAt: Date, tags: string[]
-  }) => {
-  return ({
+const generateJsonLD = ({
+  title,
+  description,
+  thumbnail,
+  updatedAt,
+  tags,
+}: {
+  title: string;
+  description: string;
+  thumbnail: string;
+  updatedAt: Date;
+  tags: string[];
+}) => {
+  const DOMANE_NAME = "https://kmin283.com";
+  return {
     "@context": "https://schema.org/",
     "@type": "BlogPosting",
     headline: title,
     image: thumbnail,
     keywords: tags,
     description,
-    url: `https://kmin.com/${title}`,
+    url: `${DOMANE_NAME}/${title}`,
     author: {
       "@type": "Person",
       name: "kmin",
-      url: "https://kmin.com"
+      url: DOMANE_NAME,
     },
     editor: {
       "@type": "Person",
       name: "kmin",
-      url: "https://kmin.com"
+      url: DOMANE_NAME,
     },
     publisher: {
       "@type": "Person",
       name: "kmin",
-      url: "https://kmin.com"
+      url: DOMANE_NAME,
     },
     dateCreated: updatedAt,
     datePublished: updatedAt,
     dateModified: updatedAt,
-    
-  });
+  };
 };
 
 export default generateJsonLD;
--- scripts/src/sitemap.js
+++ scripts/src/sitemap.js
@@ -1,11 +1,11 @@
-import {globby} from "globby";
-import {writeFileSync} from "fs";
-import prettier from 'prettier';
-import getImageMetadata from './getImageMetadata.js';
+import { globby } from "globby";
+import { writeFileSync } from "fs";
+import prettier from "prettier";
+import getImageMetadata from "./getImageMetadata.js";
 
 const getDate = new Date().toISOString().slice(0, 10);
-const DOMAIN_NAME = "https://kmin.com";
-const formatted = sitemap => prettier.format(sitemap, {parser: "html"});
+const DOMAIN_NAME = "https://kmin283.com";
+const formatted = (sitemap) => prettier.format(sitemap, { parser: "html" });
 
 (async () => {
   const pages = await globby([
@@ -16,7 +16,6 @@ const formatted = sitemap => prettier.format(sitemap, {parser: "html"});
     "!../project/pages/_blog-admin/**/*.tsx",
   ]);
   const markdowns = await globby(["../project/mds/*"]);
-  //TODO http://localhost:3000 하드코딩 된 부분 나중에 도메인으로 변경
   const pageSitemap = `
   ${pages
     .map((page) => {
@@ -27,36 +26,38 @@ const formatted = sitemap => prettier.format(sitemap, {parser: "html"});
       const routePath = path === "index" ? "" : path;
       return `
     <url>
-    <loc>http://localhost:3000/${routePath}</loc>
+    <loc>${DOMAIN_NAME}/${routePath}</loc>
     <lastmod>${getDate}</lastmod>
 </url>`;
     })
     .join("")}`;
 
-  //TODO markdown 파일에 맞춰서 그 안에 <image태그를> url안에 넣어야 함.
-
   const imageMetadata = await getImageMetadata();
 
   const markdownSitemap = `
-  ${markdowns.map((markdown, index) => {
-    const imageSitemap = `${imageMetadata[index].map(({alt, source, title}) => {
-      return `<image:image>
+  ${markdowns
+    .map((markdown, index) => {
+      const imageSitemap = `${imageMetadata[index]
+        .map(({ alt, source, title }) => {
+          return `<image:image>
       <image:loc>http://example.com${source}</image:loc>
       <image:caption>${alt}</image:caption>
       <image:title>${title}</image:title>
     </image:image>`;
-    }).join("")}`;
-    const path = markdown.replace("../project/mds/", "").replace(".md", "");
-    return `<url>
-      <loc>http://localhost:3000/${path}</loc>
+        })
+        .join("")}`;
+      const path = markdown.replace("../project/mds/", "").replace(".md", "");
+      return `<url>
+      <loc>${DOMAIN_NAME}/${path}</loc>
       <lastmod>${getDate}</lastmod>
       ${imageSitemap}
     </url>`;
-  }).join("")}`;
-
+    })
+    .join("")}`;
 
   const generatedSitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">>
     ${pageSitemap}
     ${markdownSitemap}
   </urlset>`;
