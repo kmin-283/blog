c609ac4acff986cb81b76a2f81db77d40cc405ae
kmin-283 <rudgks1203@naver.com>
2021-11-17 22:10:03 +0900

    feat: Tab Component스타일링

--- project/components/Tabs/Tab/Tab.tsx
+++ project/components/Tabs/Tab/Tab.tsx
@@ -4,11 +4,20 @@ import styles from "./Tab.module.css";
 export interface TabProps {
   tabId: string;
   active: boolean;
+  onClick: (tabId: string) => React.MouseEventHandler<HTMLLIElement>;
 }
 
-const Tab: FC<TabProps> = ({active, tabId, children}) => {
+const Tab: FC = ({children, ...props}) => {
+  const { active, tabId, onClick } = props as TabProps;
+
   return (
-    <li className={`${styles.tab} ${active ? styles.active : ""}`} role="tab" aria-selected={active} aria-controls={tabId}>
+    <li
+      className={`${styles.tab} ${active ? styles.active : ""}`}
+      role="tab"
+      aria-selected={active}
+      aria-controls={`${tabId}-tab`}
+      onClick={onClick(tabId)}
+    >
       {children}
     </li>
   );
