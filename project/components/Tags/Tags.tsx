172f52473928287999a8df1ed01e873df451b927
yalpal yappala <rudgks114@gmail.com>
2021-11-20 22:00:47 +0900

    feat: Tags 컴포넌트 수정

--- project/components/Tags/Tags.tsx
+++ project/components/Tags/Tags.tsx
@@ -1,27 +1,32 @@
-import React, { FC } from "react";
+import React, {FC, useRef} from "react";
 import styles from "./Tags.module.css";
+import {generateRandomColors, getContrastYIQs} from "@/utils/color";
 
 interface TagsProps {
   tags: string[];
   howMany: number;
-  deleteTag?: (index: number) => void;
+  onClickHandler?: (index: number) => void;
 }
 
-const Tags: FC<TagsProps> = ({ tags, howMany, deleteTag }) => {
+const Tags: FC<TagsProps> = ({ tags, howMany, onClickHandler} ) => {
   const handleOnClick = (index: number) => () => {
-    if (deleteTag) {
-      deleteTag(index);
+    if (onClickHandler) {
+      onClickHandler(index);
     }
   };
+  const randomColors = useRef<string[]>(generateRandomColors(howMany));
+  const textColors = useRef<string[]>(getContrastYIQs(randomColors.current));
+  
   return (
     <ul className={styles.tags}>
       {tags.slice(0, howMany).map((tag, index) => (
         <li
-          className={styles.tag}
+          className={`${styles.tag} ${onClickHandler ? styles.shrink : ''}`}
           key={`${tag}-${index}`}
+          style={{backgroundColor: randomColors.current[index], color: textColors.current[index]}}
           onClick={handleOnClick(index)}
         >
-          {tag}
+          <p>{tag}</p>
         </li>
       ))}
     </ul>
