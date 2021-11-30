6a06fb35e6251e6554b64479e59995f179420d12
yalpal yappala <rudgks114@gmail.com>
2021-12-01 02:01:35 +0900

    feat: lib alias 적용

--- project/.storybook/main.js
+++ project/.storybook/main.js
@@ -1,14 +1,11 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  "stories": [
+  stories: [
     "../components/**/*.stories.mdx",
-    "../components/**/*.stories.@(js|jsx|ts|tsx)"
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   webpackFinal: async (baseConfig, options) => {
     // Modify or replace config. Mutating the original reference object can cause unexpected bugs.
     const { module = {} } = baseConfig;
@@ -17,35 +14,36 @@ module.exports = {
       ...baseConfig,
       module: {
         ...module,
-        rules: [...(module.rules || [])]
-      }
+        rules: [...(module.rules || [])],
+      },
     };
 
     newConfig.resolve.alias = {
       ...baseConfig.resolve.alias,
-      '@/components': path.resolve(__dirname, "../components"),
-      '@/styles:': path.resolve(__dirname, "../styles"),
-      '@/utils': path.resolve(__dirname, '../utils')
+      "@/components": path.resolve(__dirname, "../components"),
+      "@/styles:": path.resolve(__dirname, "../styles"),
+      "@/utils": path.resolve(__dirname, "../utils"),
+      "@/libs": path.resolve(__dirname, "../libs"),
     };
 
     // TypeScript with Next.js
     newConfig.module.rules.push({
       test: /\.(ts|tsx)$/,
       include: [
-        path.resolve(__dirname, '../components'),
-        path.resolve(__dirname, '../stories')
+        path.resolve(__dirname, "../components"),
+        path.resolve(__dirname, "../stories"),
       ],
       use: [
         {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['next/babel'],
-            plugins: ['react-docgen']
-          }
-        }
-      ]
+            presets: ["next/babel"],
+            plugins: ["react-docgen"],
+          },
+        },
+      ],
     });
-    newConfig.resolve.extensions.push('.ts', '.tsx');
+    newConfig.resolve.extensions.push(".ts", ".tsx");
 
     //
     // CSS Modules
@@ -54,25 +52,25 @@ module.exports = {
 
     // First we prevent webpack from using Storybook CSS rules to process CSS modules
     newConfig.module.rules.find(
-      rule => rule.test.toString() === '/\\.css$/'
+      (rule) => rule.test.toString() === "/\\.css$/"
     ).exclude = /\.module\.css$/;
 
     // Then we tell webpack what to do with CSS modules
     newConfig.module.rules.push({
       test: /\.module\.css$/,
-      include: path.resolve(__dirname, '../components'),
+      include: path.resolve(__dirname, "../components"),
       use: [
-        'style-loader',
+        "style-loader",
         {
-          loader: 'css-loader',
+          loader: "css-loader",
           options: {
             importLoaders: 1,
-            modules: true
-          }
-        }
-      ]
+            modules: true,
+          },
+        },
+      ],
     });
 
     return newConfig;
-  }
+  },
 };
