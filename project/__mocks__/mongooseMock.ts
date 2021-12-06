1329170bab2ff4ea1fed0f3799a6fc067db86caa
kmin-283 <rudgks1203@naver.com>
2021-12-07 00:50:14 +0900

    refactor & test: database모듈, 테스트 완료

--- project/__mocks__/mongooseMock.ts
+++ project/__mocks__/mongooseMock.ts
@@ -1,4 +1,6 @@
-import { Schema } from "mongoose";
+import { Connection, Schema } from "mongoose";
+import { ReturnState } from "@/libs/Database";
+import { IPost } from "@/models/post";
 
 type post = {
   _id: string;
@@ -14,7 +16,7 @@ const mockModel = jest.fn(
       clear: () => {
         posts = [];
       },
-      find: async (filter: {}): Promise<documents<post>[]> => {
+      find: async (filter: {}) => {
         return posts;
       },
       deleteOne: async ({ _id: __id }: { _id: string }) => {
@@ -40,4 +42,116 @@ const mockModel = jest.fn(
   }
 );
 
-export { mockModel };
+const mockConnect = jest.fn((uri: string, dbName: string) => {
+  if (uri === "mongodb://localhost:27017/test") {
+    return {
+      db: jest.fn().mockReturnThis(),
+      model: mockModel,
+    } as unknown as Connection;
+  }
+  return null;
+});
+
+const mockCreate = jest.fn(
+  async <T>({
+    document,
+    modelName,
+    modelSchema,
+  }: {
+    document: { [P in keyof T]?: T[P] };
+    modelName: string;
+    modelSchema: Schema;
+  }): Promise<ReturnState<IPost>> => {
+    if (modelName === "FailToCreate") {
+      return { success: false };
+    }
+    await mockModel("name", {} as Schema, "collections").create(document);
+    return { success: true };
+  }
+);
+
+const mockFind = jest.fn(
+  async <T>({
+    modelName,
+    modelSchema,
+  }: {
+    modelName: string;
+    modelSchema: Schema;
+  }): Promise<ReturnState<T>> => {
+    if (modelName === "FailToFind") {
+      return { success: false };
+    }
+    const data = await mockModel("name", {} as Schema, "collections").find({});
+    return { success: true, data: data as unknown as T[] };
+  }
+);
+const mockDeleteOne = jest.fn(
+  async <T>({
+    filter,
+    modelName,
+    modelSchema,
+  }: {
+    filter: { _id: string };
+    modelName: string;
+    modelSchema: Schema;
+  }): Promise<ReturnState<T>> => {
+    if (modelName === "FailToDelete") {
+      return { success: false };
+    }
+    await mockModel("name", {} as Schema, "collections").deleteOne(filter);
+    return { success: true };
+  }
+);
+
+const mockFindById = jest.fn(
+  async <T>({
+    id,
+    modelName,
+    modelSchema,
+  }: {
+    id: string;
+    modelName: string;
+    modelSchema: Schema;
+  }): Promise<ReturnState<T>> => {
+    if (modelName === "FailToFindById") {
+      return { success: false };
+    }
+    const data = await mockModel("name", {} as Schema, "collections").findById(
+      id
+    );
+    return { success: true, data: data as unknown as T };
+  }
+);
+const mockFindByIdAndUpdate = jest.fn(
+  async <T>({
+    id,
+    update,
+    modelName,
+    modelSchema,
+  }: {
+    id: string;
+    update: { [P in keyof T]?: T[P] };
+    modelName: string;
+    modelSchema: Schema;
+  }): Promise<ReturnState<T>> => {
+    if (modelName === "FailToFindByIdAndUpdate") {
+      return { success: false };
+    }
+    const data = await mockModel(
+      "name",
+      {} as Schema,
+      "collections"
+    ).findByIdAndUpdate(id, update);
+    return { success: true, data: data as unknown as T };
+  }
+);
+
+export {
+  mockConnect,
+  mockModel,
+  mockCreate,
+  mockFind,
+  mockDeleteOne,
+  mockFindById,
+  mockFindByIdAndUpdate,
+};
